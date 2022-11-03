0cba45d041d865395dcfda7b74473e21c65c848c
TobiasLueger <tobias.lueger98@gmail.com>
2022-11-03 22:04:19 +0100

    [TASK] new stage and naviagation

--- portfolio/Components/Header.tsx
+++ portfolio/Components/Header.tsx
@@ -1,34 +1,49 @@
-import React from "react";
+import React, { useState } from "react";
 import Image from "next/image";
 import Link from "next/link";
+import { List } from "phosphor-react";
 
 const Header = ({}) => {
+  const [mobileClicked, setMobileClicked] = useState(false);
+
   return (
-    <div className="absolute w-full">
-      <div className="bg-transparent z-[5] relative left-0 pt-0 w-full h-auto visible opacity-100 text-right">
-        <div className="max-w-[90%] px-0 mx-auto">
-          <div className="flex h-[80px]">
-            <div className="w-[40%] text-left">
-              <Link href="/">
-                <p>tobias lüger</p>
-              </Link>
-            </div>
-            <div className="w-[60%] text-right">
-              <nav className="h-full relative flex flex-col justify-end">
-                <ul className="flex-row h-full relative flex justify-end list-none">
-                  <li className="flex border-none text-left text-[#000] leading-[2.4] text-[15px] tracking-[-0.2px] font-[600]">
-                    <Link href="#work">work</Link>
-                  </li>
-                  <li className="flex border-none text-left text-[#000] leading-[2.4] text-[15px] tracking-[-0.2px] font-[600]">
-                    <Link href="/about-me">about me</Link>
-                  </li>
-                  <li className="flex border-none text-left text-[#000] leading-[2.4] text-[15px] tracking-[-0.2px] font-[600]">
-                    <Link href="mailto:tobias-lueger@web.de">get in touch</Link>
-                  </li>
-                </ul>
-              </nav>
-            </div>
-          </div>
+    <div className="absolute w-full flex flex-row justify-between items-end h-[60px] px-[30px]">
+      <div className="flex flex-row w-full justify-between items-center">
+        <Link href="/">
+          <p className="font-bold text-2xl text-black flex relative">
+            tobias lüger
+            <Image
+              src="/img/portfolio.png"
+              className="max-w-[30px] relative -top-3 -right-2"
+              alt={""}
+              width="920"
+              height="920"
+            ></Image>
+          </p>
+        </Link>
+        <div className="md:hidden">
+          <List
+            size={35}
+            color="#000"
+            weight="bold"
+            onClick={() => setMobileClicked(!mobileClicked)}
+          />
+        </div>
+
+        <div className={`md:flex ${mobileClicked ? "flex" : "hidden"}`}>
+          <nav className="h-full relative flex flex-col justify-end">
+            <ul className="flex-row h-full relative flex justify-end list-none">
+              <li className="flex border-none text-left text-[#0f0f0f] leading-[2.4] text-[15px] tracking-[-0.2px] font-[600]">
+                <Link href="#work">work</Link>
+              </li>
+              <li className="flex border-none text-left text-[#0f0f0f] leading-[2.4] text-[15px] tracking-[-0.2px] font-[600]">
+                <Link href="/about-me">about me</Link>
+              </li>
+              <li className="flex border-none text-left text-[#0f0f0f] leading-[2.4] text-[15px] tracking-[-0.2px] font-[600]">
+                <Link href="mailto:tobias-lueger@web.de">get in touch</Link>
+              </li>
+            </ul>
+          </nav>
         </div>
       </div>
     </div>
--- portfolio/pages/index.tsx
+++ portfolio/pages/index.tsx
@@ -1,6 +1,7 @@
 import type { NextPage } from "next";
 import Head from "next/head";
 import Image from "next/image";
+import Link from "next/link";
 import Header from "../Components/Header";
 import Me from "../Components/Me";
 import Projects from "../Components/Projects";
@@ -8,7 +9,7 @@ import SocialBar from "../Components/SocialBar";
 
 const Home: NextPage = () => {
   return (
-    <div className="">
+    <div className="bg-[#f6f5f1]">
       <Head>
         <title>Tobias Lüger - Frondend Developer</title>
         <link rel="icon" href="/favicon.ico" />
@@ -16,15 +17,28 @@ const Home: NextPage = () => {
 
       <Header></Header>
 
-      <main className="flex w-full flex-1 flex-col items-center justify-center px-20 text-center">
-        <div>
-          Hi, I am Tobias. I am a frondend developer and passionate about
-          digital solutions.
+      <main className="flex w-full flex-1 flex-col items-center justify-center px-[30px]">
+        <div className="h-screen flex flex-col justify-end pb-[80px]">
+          <p className="text-4xl md:text-5xl lg:text-7xl font-bold text-black tracking-[-0.04em] mb-6">
+            Hi, I am Tobias. <br className="hidden md:block" /> I am a frondend
+            developer and passionate about digital solutions.
+          </p>
+          <div
+            onClick={() => {
+              let work = document.getElementById("work");
+              work &&
+                work.scrollIntoView({ behavior: "smooth", block: "start" });
+            }}
+            className="text-2xl border-b-[1px] border-[#a5a5a5] font-normal w-fit relative tracking-tighter flex flex-col text-[#a5a5a5] after:h-[1px] after:bg-black after:max-w-0 after:w-full after:absolute after:bottom-[-1px] hover:after:max-w-full  hover:text-black transition duration-[0.4s] ease-out cursor-pointer"
+          >
+            view my work
+          </div>
         </div>
         <div id="work">
           <div>
             <Image
               src="/img/blinddate.png"
+              className="max-w-[120px]"
               alt={""}
               width="920"
               height="920"
@@ -39,6 +53,7 @@ const Home: NextPage = () => {
           <div>
             <Image
               src="/img/wier.jpeg"
+              className="max-w-[120px]"
               alt={""}
               width="920"
               height="920"
@@ -53,6 +68,7 @@ const Home: NextPage = () => {
           <div>
             <Image
               src="/img/portfolio.png"
+              className="max-w-[120px]"
               alt={""}
               width="920"
               height="920"
